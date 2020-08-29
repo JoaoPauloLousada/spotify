d723994f18f9107e938ff1046639e8142ccbaa9d
Joao Paulo Lousada <jplousada@hotmail.com>
2020-08-28 22:38:10 -0300

    update spotify app

---
+++ spotify/src/reducer.js
@@ -0,0 +1,33 @@
+export const initialState = {
+  user: null,
+  playlists: [],
+  playing: false,
+  item: null,
+  // token:
+  // "BQBp0wkOy3rreIfxKLxQAJ2s2iXTfruClQ55nh5MxyFFlCPkU5hDt28FMIl-XLGd-91Vd23SCDP6MKe0qVqIQIQulToQAtwhSY8Lr7O0rIuSXCyef9bMURfzTNv0H3ZzPbTla4_FehxWj77gY9rpjwBYrbok5xcM6uxZRFfRIodbHFzl",
+};
+
+const reducer = (state, action) => {
+  console.log(action);
+  switch (action.type) {
+    case "SET_USER":
+      return {
+        ...state,
+        user: action.user,
+      };
+    case "SET_TOKEN":
+      return {
+        ...state,
+        token: action.token,
+      };
+    case "SET_PLAYLISTS":
+      return {
+        ...state,
+        playlists: action.playlists,
+      };
+    default:
+      return state;
+  }
+};
+
+export default reducer;
